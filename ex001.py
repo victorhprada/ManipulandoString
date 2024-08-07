6ec00521e86366f774f05ada62d6eef675b70167
victo <victorhugopteixeira@hotmail.com>
2024-08-06 21:41:32 -0300

    Exercicios de string

---
+++ ex001.py
@@ -0,0 +1,10 @@
+nome = 'Victor Hugo Prada Teixeira'
+
+print(nome.upper())
+print(nome.lower())
+print(len(nome.strip()))
+new = nome.replace(' ', '')
+print(len(new))
+print(nome.split())
+dividido = nome.split()
+print(len(dividido[0]))
